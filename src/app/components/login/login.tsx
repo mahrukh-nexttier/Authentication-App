6af298c3a0c99cc63dd68e2ebe194f9a27da8c8c
mahrukh-nexttier <mahrukh.sohail@nexttier.io>
2024-01-03 13:13:19 +0500

    api of login and sigup integrated

--- src/app/components/login/login.tsx
+++ src/app/components/login/login.tsx
@@ -4,33 +4,88 @@ import Image from "next/image";
 import React, { useState, useEffect } from "react";
 import { usePathname } from "next/navigation";
 export default function Login() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
+  const [email, setName] = useState("");
   const [password, setPassword] = useState("");
+  let [reply, setreply] = useState("");
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (password.trim().length <= 5) {
+      console.log("Invalid Password");
+      return;
+    } else if (email.trim().length === 0) {
+      console.log("Email is required");
+      return;
+    } else {
+      try {
+        const response = await fetch(
+          "https://30a8-2407-aa80-314-b88f-e4a9-198e-e08f-9715.ngrok-free.app/api/login",
+          {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ email, password }),
+          }
+        );
+        if (response.ok) {
+          console.log("form is submitted");
+          setreply("Login successful");
+        } else {
+          console.log("Login failed");
+          setreply("Login failed");
+        }
+      } catch (error) {
+        console.error("Error during login", error);
+      }
+    }
+  };
   return (
     <>
       <div className="">
         <div className="flex justify-center items-center">
-          <form className="w-[400px] h-[400px] mt-[100px] bg-gray-400 rounded-[10px] p-[10px]">
+          <form
+            className="w-[400px] h-[400px] mt-[100px] bg-gray-400 rounded-[10px] p-[10px]"
+            onSubmit={handleSubmit}
+          >
+            <p className="font-bold text-[20px] text-black text-center">
+              Log In
+            </p>
             <input
+              value={email}
               className="w-full text-black text=[12px] h-[30px] mt-[50px] rounded-[20px] pl-[20px]"
-              type="text"
-              id="name"
+              type="email"
+              id="email"
               placeholder="What is your name?"
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
             />
             <br></br>
 
             <input
+              value={password}
               className="w-full text-black text=[12px] mt-[30px] h-[30px] rounded-[20px] pl-[20px]"
               type="password"
               id="password"
               placeholder="Your password?"
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
             />
             <div className="item-center justify-center flex mt-[60px]">
-              <button className="w-[200px] bg-blue-500 rounded-[10px] h-[30px]">
+              <button
+                type="submit"
+                className="w-[200px] bg-blue-500 rounded-[10px] h-[30px]"
+              >
                 Login
               </button>
             </div>
+
+            <p
+              className="text-center items-center text-black font-bold mt-[10px]
+            "
+            >
+              {" "}
+              {reply}
+            </p>
           </form>
         </div>
       </div>
--- src/app/components/signup/signup.tsx
+++ src/app/components/signup/signup.tsx
@@ -5,30 +5,87 @@ export default function Signup() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [reply, setreply] = useState("");
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (password.trim().length <= 5) {
+      console.log("Invalid Password");
+    } else if (email.trim().length === 0) {
+      console.log("Email is required");
+    } else {
+      try {
+        const response = await fetch(
+          "https://30a8-2407-aa80-314-b88f-e4a9-198e-e08f-9715.ngrok-free.app/api/register",
+          {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ name, email, password }),
+          }
+        );
+        if (response.ok) {
+          console.log("form is submitted");
+          setreply("User has been registered successfully");
+        } else {
+          console.log("Login failed");
+          setreply("Error while creating User");
+        }
+      } catch (error) {
+        console.error("Error during login", error);
+      }
+    }
+  };
   return (
     <>
       <div className="">
         <div className="flex justify-center items-center">
-          <form className="w-[400px] h-[400px] mt-[100px] bg-gray-400 rounded-[10px]  p-[10px]">
+          <form
+            className="w-[400px] h-[400px] mt-[100px] bg-gray-400 rounded-[10px]  p-[10px]"
+            onSubmit={handleSubmit}
+          >
+            <p className="font-bold text-[20px] text-black text-center">
+              Sign Up
+            </p>
             <input
               className="w-full text-black text=[12px] h-[30px] mt-[50px] rounded-[20px] pl-[20px]"
               type="text"
               id="name"
               placeholder="What is your name?"
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
             />
             <br></br>
-
+            <input
+              className="w-full text-black text=[12px] mt-[30px] h-[30px] rounded-[20px] pl-[20px]"
+              type="email"
+              id="email"
+              placeholder="Your email?"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+            <br></br>
             <input
               className="w-full text-black text=[12px] mt-[30px] h-[30px] rounded-[20px] pl-[20px]"
               type="password"
               id="password"
               placeholder="Your password?"
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
             />
             <div className="item-center justify-center flex mt-[60px]">
               <button className="w-[200px] bg-blue-500 rounded-[10px] h-[30px]">
                 Signup
               </button>
             </div>
+            <p
+              className="text-center items-center text-black font-bold mt-[10px]
+            "
+            >
+              {" "}
+              {reply}
+            </p>
           </form>
         </div>
       </div>
